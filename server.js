80d3c9abaa6cc70f9a27517ec298eae041bcb1c8
Hakan Urhan <hakan@Hakan-MacBook-Air.local>
2025-12-16 06:17:46 +0300

    Server final fix

--- server.js
+++ server.js
@@ -209,4 +209,4 @@ app.post('/api/softtr-orders', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`🚀 Sunucu Port ${PORT} üzerinde çalışıor`));
+app.listen(PORT, () => console.log(`🚀 Sunucu Port ${PORT} üzerinde çalışıyor`));
